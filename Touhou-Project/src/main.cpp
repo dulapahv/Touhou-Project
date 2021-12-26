00c0511dccf5dc5ce0f19c16b4a2a345610ef763
Dulapah Vibulsanti <dulapah6@gmail.com>
2021-12-26 20:14:58 +0700

    Update
    - Add sound and SFX assets
    - Add title menu sound
    - Add todo list

---
+++ Touhou-Project/src/main.cpp
@@ -0,0 +1,46 @@
+﻿#include "raylib.h"
+
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 1024;
+    const int screenHeight = 768;
+
+    InitWindow(screenWidth, screenHeight, "Touhou Project by n0miya");
+
+    InitAudioDevice();              // Initialize audio device
+
+    Music music = LoadMusicStream("resources/music/TITLE.wav");
+
+    PlayMusicStream(music);
+
+    SetTargetFPS(360);
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())
+    {
+        // Update music buffer with new stream data
+        UpdateMusicStream(music);
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+        DrawText("Touhou Project by n0miya", 250, 150, 40, GRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    UnloadMusicStream(music);   // Unload music stream buffers from RAM
+    CloseAudioDevice();         // Close audio device (music streaming is automatically stopped)
+    CloseWindow();              // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}

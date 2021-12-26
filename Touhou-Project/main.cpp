7245efc6f3c60365ce525737ca30521f783b12a2
Dulapah Vibulsanti <dulapah6@gmail.com>
2021-12-26 10:57:27 +0700

    Remove unnecessary files and add SFX

--- Touhou-Project/main.cpp
+++ Touhou-Project/main.cpp
@@ -1,32 +1,11 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic window
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute raylib_compile_execute script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   This example has been created using raylib 1.0 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2013-2016 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
 
 int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1024;
+    const int screenHeight = 768;
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 

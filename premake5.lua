5d0d418e443f9101592614c68632975b91039946
Dulapah Vibulsanti <dulapah6@gmail.com>
2021-12-26 10:09:58 +0700

    Import Raylib library and create test program

---
+++ premake5.lua
@@ -0,0 +1,109 @@
+newoption 
+{
+   trigger = "opengl43",
+   description = "use OpenGL 4.3"
+}
+
+workspace "Touhou-Project"
+	configurations { "Debug","Debug.DLL", "Release", "Release.DLL" }
+	platforms { "x64", "x86"}
+
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		symbols "On"
+		
+	filter "configurations:Debug.DLL"
+		defines { "DEBUG" }
+		symbols "On"
+
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "On"	
+		
+	filter "configurations:Release.DLL"
+		defines { "NDEBUG" }
+		optimize "On"	
+		
+	filter { "platforms:x64" }
+		architecture "x86_64"
+		
+	targetdir "bin/%{cfg.buildcfg}/"
+
+project "raylib"
+		filter "configurations:Debug.DLL OR Release.DLL"
+			kind "SharedLib"
+			defines {"BUILD_LIBTYPE_SHARED"}
+			
+		filter "configurations:Debug OR Release"
+			kind "StaticLib"
+			
+		filter "action:vs*"
+			defines{"_WINSOCK_DEPRECATED_NO_WARNINGS", "_CRT_SECURE_NO_WARNINGS"}
+			characterset ("MBCS")
+		
+		filter "system:windows"
+			defines{"_WIN32"}
+			links {"winmm", "kernel32", "opengl32", "kernel32", "gdi32"}
+			
+		filter "system:linux"
+			links {"pthread", "GL", "m", "dl", "rt", "X11"}
+			
+		filter{}
+		
+		defines{"PLATFORM_DESKTOP"}
+		if (_OPTIONS["opengl43"]) then
+			defines{"GRAPHICS_API_OPENGL_43"}
+		else
+			defines{"GRAPHICS_API_OPENGL_33"}
+		end
+	
+		location "build"
+		language "C"
+		targetdir "bin/%{cfg.buildcfg}"
+		
+		includedirs { "raylib/src", "raylib/src/external/glfw/include"}
+		vpaths 
+		{
+			["Header Files"] = { "raylib/src/**.h"},
+			["Source Files/*"] = {"raylib/src/**.c"},
+		}
+		files {"raylib/src/*.h", "raylib/src/*.c"}
+		
+project "Touhou-Project"
+	kind "ConsoleApp"
+	location "%{wks.name}"
+	language "C++"
+	targetdir "bin/%{cfg.buildcfg}"
+	cppdialect "C++17"
+	
+	vpaths 
+	{
+		["Header Files"] = { "**.h"},
+		["Source Files"] = {"**.c", "**.cpp"},
+	}
+	files {"%{wks.name}/**.c", "%{wks.name}/**.cpp", "%{wks.name}/**.h"}
+
+	links {"raylib"}
+	
+	includedirs { "%{wks.name}", "raylib/src" }
+	
+	defines{"PLATFORM_DESKTOP"}
+	if (_OPTIONS["opengl43"]) then
+		defines{"GRAPHICS_API_OPENGL_43"}
+	else
+		defines{"GRAPHICS_API_OPENGL_33"}
+	end
+	
+	filter "action:vs*"
+		defines{"_WINSOCK_DEPRECATED_NO_WARNINGS", "_CRT_SECURE_NO_WARNINGS", "_WIN32"}
+		dependson {"raylib"}
+		links {"raylib.lib"}
+        characterset ("MBCS")
+		
+	filter "system:windows"
+		defines{"_WIN32"}
+		links {"winmm", "kernel32", "opengl32", "kernel32", "gdi32"}
+		libdirs {"bin/%{cfg.buildcfg}"}
+		
+	filter "system:linux"
+		links {"pthread", "GL", "m", "dl", "rt", "X11"}
